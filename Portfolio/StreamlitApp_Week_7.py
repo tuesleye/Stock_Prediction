f56ed788f2a7ddab821849559ba2e09da4417ced
tuesleye <tuesleye@gmail.com>
2026-03-26 15:36:41 -0500

    Refactor imports and update model keys in Streamlit app

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,7 +59,7 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["AAPL", "MPWR"],
+        "keys": ["LULU", "ROK"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.00} for k in ["AAPL", "MPWR"]]
 }
 
